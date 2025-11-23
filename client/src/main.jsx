010227123bbb8a3c2e5ad69a5170a948be5640c4
sameera-87 <sameekumarasinghe87@gmail.com>
2025-11-23 13:48:19 +0530

    Home page is almost completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,3 +8,6 @@ createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>,
 )
+
+
+// Time : 1.20.03
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <FeaturedSection />
+    </>
+  )
+}
+
+export default Home
