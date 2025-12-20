a2c9c8f995c39649c68b8ce54dfcc5b5d178c041
sameera-87 <sameekumarasinghe87@gmail.com>
2025-12-20 12:12:54 +0530

    Add car function implemented

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
 
 // Initialize Express app
 const app = express()
@@ -15,7 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req,res)=> res.send("Server is running"))
-app.use('/api/user', userRouter);
+app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
